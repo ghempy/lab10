43279510c6ea32eddb12bce25d23fbe2442b00c9
Garrett Hempy <gahe0404@colorado.edu>
2018-11-07 15:52:00 -0700

    YEEEET

--- database.js
+++ database.js
@@ -6,9 +6,9 @@ var dbConfig = process.env.DATABASE_URL;
    host: 'localhost',
    port: 5432,
    database: 'lab6',
-   user: 'gary',
-   password: '1234' // TODO: Fill in your PostgreSQL password here.
-                // Use empty string if you did not set a password
+   user: 'postgres',
+   password: '' // Fill in your PostgreSQL password here. Use empty string
+                // if you did not set a password
 };*/
 
 var db = pgp(dbConfig);
