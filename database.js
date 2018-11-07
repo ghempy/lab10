f9f5da81d2675a567d411bb1126141f87d4b93c3
Garrett Hempy <gahe0404@colorado.edu>
2018-11-07 15:13:17 -0700

    First Commit

---
+++ database.js
@@ -0,0 +1,16 @@
+var pgp = require('pg-promise')();
+
+var dbConfig = process.env.DATABASE_URL;
+
+/*const dbConfig = {
+   host: 'localhost',
+   port: 5432,
+   database: 'lab6',
+   user: 'gary',
+   password: '1234' // TODO: Fill in your PostgreSQL password here.
+                // Use empty string if you did not set a password
+};*/
+
+var db = pgp(dbConfig);
+
+module.exports = db;
